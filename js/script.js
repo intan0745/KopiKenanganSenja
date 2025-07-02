1e7c10ac24442655ee3568b1e95123f3033329eb
intan0745 <i18111801@gmail.com>
2025-07-03 06:49:09 +0700

    menata tampilan responsif

--- js/script.js
+++ js/script.js
@@ -29,4 +29,28 @@ removeItemButtons.forEach(function (button) {
   button.addEventListener('click', function () {
     this.parentElement.remove();
   });
+// Modal Box
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButtons = document.querySelectorAll('.item-detail-button');
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = 'flex';
+    e.preventDefault();
+  };
+});
+
+// klik tombol close modal
+document.querySelector('.modal .close-icon').onclick = (e) => {
+  itemDetailModal.style.display = 'none';
+  e.preventDefault();
+};
+
+// klik di luar modal
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = 'none';
+  }
+};
+
 });
